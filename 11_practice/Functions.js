41ed49579f067196024074afcafeb822b9ef68ae
angel42236 <angel1vakhariya6353@gmail.com>
2023-06-28 18:38:19 +0530

    Functions

--- 11_practice/Functions.js
+++ 11_practice/Functions.js
@@ -3,10 +3,10 @@ let b = 2;
 let c = 3;
 
 function onePluseAvg(x, y){
-    return 1 + (x + y)/2 
+    return Math.round (1 + (x + y)/2) 
 }
 
-console.log("Average of a and b", onePluseAvg(a, b))
-console.log("Average b and c", 1 + (b + c )/2)
-console.log("Average a and c", 1 + (a + c)/2)
+console.log("Average of a and b is ", onePluseAvg(a, b))
+console.log("Average b and c is ",onePluseAvg(b, c))
+console.log("Average a and c is ",onePluseAvg(a, c))
  
