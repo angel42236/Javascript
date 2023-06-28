3f5203bd5be0ee62f84539dea3815936ae2fbbb8
angel42236 <angel1vakhariya6353@gmail.com>
2023-06-28 17:49:55 +0530

    For Loop & Do While Loop

---
+++ 10_practice/While_Loop.js
@@ -0,0 +1,17 @@
+// let n = ("Enter the value of n")
+// n = Number.parseInt(n)
+// let i = 0;
+// while(i<n){
+//     console.log(i)
+//     i++;
+// }
+
+// While Loopl
+let n = ("Enter the value of n")
+n = Number.parseInt(n)
+
+let i = 10;
+do{
+    console.log(i)
+    i++;
+} while (i < n)
