187e23475a5d11660defa118d0c585b976b19d9e
angel42236 <angel1vakhariya6353@gmail.com>
2023-06-28 18:47:32 +0530

    Add Function

--- 11_practice/Functions.js
+++ 11_practice/Functions.js
@@ -1,12 +1,22 @@
-let a = 1;
-let b = 2;
-let c = 3;
-
-function onePluseAvg(x, y){
-    return Math.round (1 + (x + y)/2) 
-}
-
-console.log("Average of a and b is ", onePluseAvg(a, b))
-console.log("Average b and c is ",onePluseAvg(b, c))
-console.log("Average a and c is ",onePluseAvg(a, c))
- 
+const hello = () => {
+    console.log("Hey how are you. I am toh fine yaar")
+    return "hi"
+  }
+  
+  function onePlusAvg(x, y) {
+    return 1 + (x + y) / 2
+  }
+  
+  const sum = (p, q) => {
+    return p + q
+  }
+  
+  let a = 1;
+  let b = 2;
+  let c = 3;
+  let v = hello();
+  console.log(v)
+  console.log("One plus Average of a and b is ", onePlusAvg(a, b))
+  console.log("One plus Average of b and c is ", onePlusAvg(b, c))
+  console.log("One plus Average of a and c is ", onePlusAvg(a, c))
+  console.log(sum(9, 7))
