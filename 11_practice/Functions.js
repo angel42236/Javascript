6e6bf292d3871b2b763248cb58847a56b0ddc1d5
angel42236 <angel1vakhariya6353@gmail.com>
2023-06-28 18:23:28 +0530

    Functions

---
+++ 11_practice/Functions.js
@@ -0,0 +1,12 @@
+let a = 1;
+let b = 2;
+let c = 3;
+
+function onePluseAvg(x, y){
+    return 1 + (x + y)/2 
+}
+
+console.log("Average of a and b", onePluseAvg(a, b))
+console.log("Average b and c", 1 + (b + c )/2)
+console.log("Average a and c", 1 + (a + c)/2)
+ 
