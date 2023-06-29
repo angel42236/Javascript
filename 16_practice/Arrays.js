41fd5b1751c72319e3396abdf3e846021971aa14
angel42236 <angel1vakhariya6353@gmail.com>
2023-06-29 14:39:25 +0530

    Arrays

---
+++ 16_practice/Arrays.js
@@ -0,0 +1,13 @@
+let marks_class_12 = [92, 85, 63, 56, null]
+
+console.log(marks_class_12[0])
+console.log(marks_class_12[1])
+console.log(marks_class_12[2])
+console.log(marks_class_12[3])
+console.log(marks_class_12[4])
+console.log(marks_class_12[5])
+console.log(marks_class_12.length)
+marks_class_12[5] = 89 // Adding a new value to the array
+marks_class_12[0] = 96 // Changing the value of an array
+console.log(marks_class_12)
+console.log(typeof marks_class_12)
