b6ce331f132cde9ea59045b5f8c9a97bcf1aae0d
angel42236 <angel1vakhariya6353@gmail.com>
2023-06-29 12:30:05 +0530

    String method

---
+++ 14_practice/String_methods.js
@@ -0,0 +1,17 @@
+let name = "Angel"
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.slice(0,2))
+// console.log(name.slice(2))
+// console.log(name.replace("Ang", "Bng"))
+
+let friend = "Riya"
+console.log(name.concat(" is a friend of ", friend, " ok "))
+let friend2 = "   Dhruvisha   "
+console.log(friend2.trim())
+let fr = "Ridhi" + "Ann" + "PC"
+console.log(fr[0])
+console.log(fr[1])
+console.log(fr[2])
+console.log(fr)
